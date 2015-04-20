88efe6ddbad804261d3645b9ead18a05fa2257d0
florent <florent.baudon@gmail.com>
2015-04-20 08:42:58 +0200

    Fist commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var app = express();
+
+//enabling cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/', urlencodedParser, function(req, res){
+
+});
+
+app.get('/', function(req, res){
+
+});
+
+
+app.use(function(req, res, next){
+	res.setHeader('Content-Type', 'text/plain');
+	res.status(404).send('Page not found');
+})
+
+app.listen(8000);
