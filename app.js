f0c30c53f7ed047d5cafd334d472d18c033891ff
Florent Baudon <florent.baudon@gmail.com>
2016-02-02 11:48:55 +0100

    new template

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express.js');
 
 var app = express();
 
 var port = 8000;
 
-//enabling cors
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.post('/', urlencodedParser, function(req, res){
-
-});
-
-app.get('/', function(req, res){
-
-});
-
-
-app.use(function(req, res, next){
-	res.setHeader('Content-Type', 'text/plain');
-	res.status(404).send('Page not found');
-})
-
 app.listen(port);
-console.log('Server listen on : ' + port)
+console.log('Server listen on : ' + port);
+
+
